2ae92c94cdb4e3eada53ee45c33acb2e810de261
Migue26 <68603887+Migue26@users.noreply.github.com>
2021-03-12 14:51:16 -0500

    Add files via upload

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+   public Rigidbody rb;
+   public float impulseForce = 3f;
+
+   private bool ignoreNextcollision;
+
+   //g prosicion
+   private Vector3 starPosition;
+
+   
+   public int perfectPass;
+
+   public float superSpeed = 8;
+
+   private bool isSuperSpeedActive;
+
+   public int perfectPassCount = 1;
+
+   private void Start()
+   {
+      starPosition = transform.position;
+   }
+   
+
+   private void OnCollisionEnter(Collision collision)
+   {
+
+      
+
+      if (ignoreNextcollision)
+      {
+          return;
+      }
+      if (isSuperSpeedActive && !collision.transform.GetComponent<GoalController>())
+      {
+          Destroy(collision.transform.parent.gameObject,0.2f);
+      }
+      else
+      {
+         DeathPart deathPart = collision.transform.GetComponent<DeathPart>();
+         if (deathPart)
+         {
+            GameManager.singleton.RestartLevel();
+         } 
+      }
+
+      
+      
+
+      rb.velocity = Vector3.zero;
+      rb.AddForce(Vector3.up*impulseForce, ForceMode.Impulse);
+
+      ignoreNextcollision = true;
+      Invoke("AllowNextCollision",0.2f);
+
+      perfectPass = 0;
+      isSuperSpeedActive = false;
+   }
+
+   private void Update()
+   {
+      if (perfectPass>=perfectPassCount && !isSuperSpeedActive)
+      {
+          isSuperSpeedActive = true;
+
+          rb.AddForce(Vector3.down*superSpeed, ForceMode.Impulse);
+      }
+   }
+
+   private void AllowNextCollision()
+   {
+      ignoreNextcollision = false;
+   }
+
+
+   public void ResetBall()
+   {
+      transform.position = starPosition;
+   }
+}
